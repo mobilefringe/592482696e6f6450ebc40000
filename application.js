a43538ef7dfe4ef81fff6ca6aaf02912a4e49e81
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-05-23 14:54:12 -0400

    St. Vital 1.0  /application.js

--- application.js
+++ application.js
@@ -500,7 +500,7 @@ function renderPromotions(container, template, collection){
             
         }
         else{
-            val.store_name = "South Oakville Centre";
+            val.store_name = "St. Vital Centre";
             val.image_url = "//codecloud.cdn.speedyrails.net/sites/5914aa456e6f642702040000/image/png/1494532252000/logo_hopedale.png";
         }
         
