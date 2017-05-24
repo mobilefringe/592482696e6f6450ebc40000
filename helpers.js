e72650b62b80a44c3ed2ad87c046ffcbd0b8a1ff
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-05-24 13:21:06 -0400

    St. Vital 1.0  /helpers.js

--- helpers.js
+++ helpers.js
@@ -1,3 +1,4 @@
+	var view_all = false;
 function init(e){
     $('<div class="modal-backdrop custom_backdrop"><img src="http://kodekloud.s3.amazonaws.com/sites/554a79236e6f64713f000000/69e8cd982124dc73de1f5a67a627ee75/loading.gif" class="" alt=""></div>').appendTo(document.body);
     
@@ -17,7 +18,7 @@ function init(e){
 	$('.accordion_header').click(function(e){
         $(this).find('i').toggleClass('fa-caret-down fa-caret-up');
 	});
-	var view_all = false;
+
 	
 	$("#hours-toggle").click(function(){
             console.log("toggled!");
