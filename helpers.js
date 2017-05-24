f236a285e254e4c88bb77b6df93a90a5373bffa8
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-05-24 13:23:03 -0400

    St. Vital 1.0  /helpers.js

--- helpers.js
+++ helpers.js
@@ -38,7 +38,17 @@ function init(e){
 
         $('input').val();
         
-        
+        	$("#hours-toggle").click(function(){
+            console.log("toggled!");
+            $("#hours-full").slideToggle();
+            if (!view_all){
+                $("#hours-toggle b").html("HIDE HOURS");
+                view_all = true;
+            } else {
+                $("#hours-toggle b").html("VIEW ALL HOURS");
+                view_all = false;
+            }
+        });
         $("#main-nav ul li.menu_item, .toggle_sub_menu").hover(
             function(){
                 $(this).find("img").show();
