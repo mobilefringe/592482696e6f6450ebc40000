4b65702daa60005c57d06988db00556c42dc25d4
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-05-24 16:36:48 -0400

    St. Vital 1.0  /helpers.js

--- helpers.js
+++ helpers.js
@@ -1,8 +1,7 @@
 $('document').ready(function() {
     
     if (window.location.pathname.indexOf("/stores") == -1) {
-        console.log("not stores");
-        $('.flexslider').flexslider({
+         $('.flexslider').flexslider({
             animation: "slide",
             controlNav: false,
             directionNav: false,        
