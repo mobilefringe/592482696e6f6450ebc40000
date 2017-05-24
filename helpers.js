0c28a749a0c37b58c7933ebcf04f51c71ab3f451
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-05-24 15:36:34 -0400

    St. Vital 1.0  /helpers.js

--- helpers.js
+++ helpers.js
@@ -1,4 +1,14 @@
 var view_all = false;
+
+if (window.location.pathname.indexOf("/stores") == -1) {
+        $('.flexslider').flexslider({
+            animation: "slide",
+            controlNav: false,
+            directionNav: false,        
+            prevText: "Previous",
+            nextText: "Next"
+        });
+    }; 
 function init(e){
     $('<div class="modal-backdrop custom_backdrop"><img src="http://kodekloud.s3.amazonaws.com/sites/554a79236e6f64713f000000/69e8cd982124dc73de1f5a67a627ee75/loading.gif" class="" alt=""></div>').appendTo(document.body);
     
