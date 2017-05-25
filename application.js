b77fcb52759b59f3e8a3a1ed91dd13aca2f28774
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-05-25 16:26:58 -0400

    St. Vital 1.0  /application.js

--- application.js
+++ application.js
@@ -812,7 +812,9 @@ function renderStoreTags(container, template, collection){
         if(val.tag !== null || val.tag !==undefined)
         {
             $.each( val.tag , function( keys, tags ) {
-                
+                if(jQuery.inArray("test", myarray) !== -1){
+                    
+                }
             });
         }
     });
