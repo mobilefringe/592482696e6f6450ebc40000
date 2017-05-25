e141a6fb875154e4970571a591a2fec12520b4f1
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-05-25 16:19:40 -0400

    St. Vital 1.0  /application.js

--- application.js
+++ application.js
@@ -802,4 +802,67 @@ function renderHomeDropdownHours(container, template, collection, type){
     $(container).html(item_rendered.join(''));
 }
 
+function renderStoreTgags(container, template, collection, type){
+    var item_list = [];
+    var item_rendered = [];
+    var template_html = $(template).html();
+    Mustache.parse(template_html);   // optional, speeds up future uses
+    var store_initial="";
+    $.each( collection , function( key, val ) {
+        if (type == "stores" || type == "category_stores"){
+            if(!val.store_front_url ||  val.store_front_url.indexOf('missing.png') > -1 || val.store_front_url.length === 0){
+                val.alt_store_front_url = "";
+            } else {
+                val.alt_store_front_url = getImageURL(val.store_front_url);    
+            }
+            
+        }
+        //var categories = getStoreCategories();
+        var current_initial = val.name[0];
+        val.cat_list = val.categories.join(',')
+        if(store_initial.toLowerCase() == current_initial.toLowerCase()){
+            val.initial = "";
+            val.show = "display:none;";
+        }
+        else{
+            val.initial = current_initial;
+            store_initial = current_initial;
+            val.show = "display:block;";
+        }
+        if(val.is_coming_soon_store == true){
+            val.coming_soon_store = "display:inline";
+        }
+        else{
+            val.coming_soon_store = "display:none";
+        }
+        if(val.is_new_store == true){
+            val.new_store = "display:inline";
+        }
+        else{
+            val.new_store = "display:none";
+        }
+        if (val.promotions.length > 0){
+            val.promotion_exist = "display:inline";
+            var store_promo = getPromotionsForIds(val.promotions).sortBy(function(o){ return o.start_date })[0];
+            if (store_promo != undefined){
+                val.promo_btn = "/promotions/" + store_promo.slug;
+            }
+        }
+        else{
+            val.promotion_exist = "display:none";
+        }
+        if(val.phone.length < 1){
+            val.phone_exist = "display:none";
+        }
+        val.block = current_initial + '-block';
+        var rendered = Mustache.render(template_html,val);
+        var upper_current_initial = current_initial.toUpperCase();
+        item_rendered.push(rendered);
+
+    });
+    
+    $(container).show();
+    $(container).html(item_rendered.join(''));
+}
+
 
