381b000aded924c129036a6203140abeb0e4dafb
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-05-25 09:43:16 -0400

    St. Vital 1.0  /helpers.js

--- helpers.js
+++ helpers.js
@@ -39,7 +39,146 @@ function init(e){
     function close_popup() {
         $(".newsletter-box").hide();
     }
+    if (window.location.pathname.indexOf("/stores") == -1) {
+        console.log(window.location.pathname,"not store");
+        $('.flexslider').flexslider({
+            animation: "slide",
+            controlNav: false,
+            directionNav: false,        
+            prevText: "Previous",
+            nextText: "Next"
+        });
+    }
     
+    var view_all = false;
+    $('#hours-toggle').click(function(){
+        console.log("toggled!");
+        $("#hours-full").slideToggle();
+        if (!view_all){
+            $("#hours-toggle b").html("HIDE HOURS");
+            view_all = true;
+        } else {
+            $("#hours-toggle b").html("VIEW ALL HOURS");
+            view_all = false;
+        }
+    });
+        var today_hours = getTodaysHours();
+        var hours = getPropertyHours();
+        var feature_items = getFeatureList();
+        var banners  = getBanners();
+        renderBanner('#banner_template','#home_banner',banners);
+        renderHomeHours('#today_hour_container', '#today_hour_template', today_hours);
+        renderHours('#home_hour_container', '#home_hour_template', hours, 'reg_hours');
+        renderHours('#home_exp_hour_container', '#home_exp_hour_template', hours, 'holiday_hours');
+        renderFeatureItems('#feature_container', '#feature_template', feature_items);
+        
+        
+        $('input').placeholder();
+        
+        
+        $("#main-nav ul li.menu_item, .toggle_sub_menu").hover(
+            function(){
+                $(this).find("img").show();
+            },function(){
+                $(this).find("img").hide();
+            }
+        );
+        
+        $(".sub_menu_li").hover(
+            function(){
+                $(this).find("ul").show();
+            }, function(){
+                $(this).find("ul").hide();
+            }
+        );
+        
+        $(".page-type-content:nth-child(4)").hover(
+            function(){
+                $("#service_hover").show();
+            }, function(){
+                $("#service_hover").hide();
+            }
+        );
+        
+        var n = 0;
+        $(".home_page h3").each(function(){
+            if (n % 4 == 1 ){
+                $(this).addClass("yellow");    
+                $(this).addClass("ph"+n);    
+            } 
+            if (n % 4 == 2 ){
+                $(this).removeClass("yellow");  
+                $(this).addClass("purple");  
+                $(this).addClass("ph"+n);    
+            }
+            if (n % 4 == 3 ){
+                $(this).removeClass("yellow");  
+                $(this).removeClass("purple");  
+                $(this).addClass("red");    
+                $(this).addClass("ph"+n);    
+            } 
+            
+            n = n+1;
+        });
+        
+        if (getURLParameter("building") == "1"){           
+            $(".building_menu").show();         
+            
+            var menu_index = getURLParameter("menu");
+            $("#building_li_"+ menu_index + " a").addClass("building_menu_highlight");
+        } else {
+             $(".main_menu").show();
+        }
+    
+    //Campaign Monitor Sign Up
+    $('#subForm').submit(function (e) {
+//        if ($("#agree_terms").prop("checked") != true){
+//            alert("Please agree to the term and conditions.");
+//            $("#agree_terms").focus();
+//            return false;
+//        }
+        e.preventDefault();
+        $.getJSON(
+            this.action + "?callback=?",
+            $(this).serialize(),
+                function (data) {
+                    if (data.Status === 400) {
+                        alert("Please try again later.");
+                    } else { // 200
+                        alert("Thank you for signing up.");
+                    }
+        });
+    });
+        
+    
+    function validate_pop_up(){
+        if($('#subscribe_newsletter_popup').is(":checked"))
+        return true;
+        else{
+            alert("Please check the 'Subscribe to recieve newsletter' checkbox")
+            return false;
+        }
+    }
+    function search_site(){
+        if($("#SearchTerms").val() != ""){
+            window.location.href = "/search?query=" + $("#SearchTerms").val();
+        }
+    }
+    
+    var visited = $.cookie("visited")
+        if (visited === null) {
+            $.magnificPopup.open({
+              items: {
+                src: '<div class="mfp-with-anim homepop_up_container" style="height:100%;"><a href=http://www.stvitalcentre.com/pages/svc-giftcard><img alt="Pop up" width="100%" src="//codecloud.cdn.speedyrails.net/sites/592482696e6f6450ebc40000/image/jpeg/1495572694000/CAN150_Popup_FNL.jpg" /></a></div>', 
+                type: 'inline'
+              }
+            });
+            $.cookie('visited', 'yes'); 
+        }
+        var date = new Date();
+        var minutes = 1440;
+        date.setTime(date.getTime() + (minutes * 60 * 1000));
+        $.cookie('visited', 'yes', { expires: date, path: '/' });
     
  
 
