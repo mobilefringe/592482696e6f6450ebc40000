c7ca7106ea6f547874f5752aff2ba721858dc38d
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-05-25 14:00:25 -0400

    St. Vital 1.0  /helpers.js

--- helpers.js
+++ helpers.js
@@ -23,7 +23,7 @@ $('document').ready(function() {
         renderFeatureItems('#feature_container', '#feature_template', feature_items);
         
         
-        $('input').val();
+        $('input').placeholder();
         
         
         $("#main-nav ul li.menu_item, .toggle_sub_menu").hover(
