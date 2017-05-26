327794275e3b7d14204c2e8d2e31075ae3634645
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-05-26 11:02:18 -0400

    St. Vital 1.0  /application.js

--- application.js
+++ application.js
@@ -807,7 +807,7 @@ function renderStoreTags(container, template, collection){
     var item_rendered = [];
     var template_html = $(template).html();
     Mustache.parse(template_html);   // optional, speeds up future uses
-    var tag_list = [];
+    //var tag_list = [];
     $.each( collection , function( key, val ) {
         if(val.tags !== null || val.tags !==undefined)
         {
