3d184723c5a989b9e6c181fb83907dec0a39cc1c
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-06-01 16:05:34 -0400

    St. Vital 1.0  /application.js

--- application.js
+++ application.js
@@ -623,8 +623,7 @@ function renderPromotions(container, template, collection){
         else{
             val.dates = start.format("MMMM D") + " - " + end.format("MMMM D")
         }
-        val.day = start.format("ddd");
-        console.log(val.day);
+        val.day = start.format("ddd").toLowerCase();
         val.month = start.format("MMM");
         val.date = start.format("DD");
         val.main_host= getPropertyDetails().mm_host;
