f3f01c07755a17555d1938c57c8eb74993c13adf
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-06-01 15:25:07 -0400

    St. Vital 1.0  /application.js

--- application.js
+++ application.js
@@ -625,6 +625,7 @@ function renderPromotions(container, template, collection){
         }
         val.day = start.format("ddd");
         val.month = start.format("MMM");
+        val.day = start.format("ddd")
         var rendered = Mustache.render(template_html,val);
         item_rendered.push(rendered);
     });
