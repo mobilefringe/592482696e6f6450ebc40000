2d068e4d3bdd57afaa63262a467f7e993325560c
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-06-01 16:34:14 -0400

    St. Vital 1.0  /helpers.js

--- helpers.js
+++ helpers.js
@@ -122,7 +122,26 @@ $(window).load(function() {
             document.cookie = 'visited=yes; expires='+a.toGMTString()+';'; 
         } 
     }
-    
+    $(".home_page h3").each(function(){
+            //console.log("loads colour");
+            if (n % 4 == 1 ){
+                $(this).addClass("yellow");    
+                $(this).addClass("ph"+n);    
+            } 
+            if (n % 4 == 2 ){
+                $(this).removeClass("yellow");  
+                $(this).addClass("purple");  
+                $(this).addClass("ph"+n);    
+            }
+            if (n % 4 == 3 ){
+                $(this).removeClass("yellow");  
+                $(this).removeClass("purple");  
+                $(this).addClass("red");    
+                $(this).addClass("ph"+n);    
+            } 
+            
+            n = n+1;
+        });
     var today_hours = getTodaysHours();
     var hours = getPropertyHours();
     var feature_items = getFeatureList();
