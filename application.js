42675c9689aace0430f676e747de3277bdfb98dc
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-06-02 17:17:13 -0400

    St. Vital 1.0  /application.js

--- application.js
+++ application.js
@@ -342,6 +342,7 @@ function renderStoreDetails(container, template, collection, slug){
         
         var repo = getRepoDetailsByName("Building Design Specs");
         $.each( repo , function( repo_key, repo_val ) {
+            console.log(repo_val);
             if(repo_val.name == val.neighbourhood.toLowerCase())
             {
                 console.log("something",repo_val.name);
