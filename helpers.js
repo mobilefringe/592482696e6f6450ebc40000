c43fb2c66a475aee0cb7f17440239e5876f417c4
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-06-02 14:24:37 -0400

    St. Vital 1.0  /helpers.js

--- helpers.js
+++ helpers.js
@@ -201,9 +201,7 @@ function show_content(){
     }
     
     var n = 0;
-	console.log("loading init(e)",n);
     $("div.home_page h3").each(function(){
-        console.log("loads colour");
         if (n % 4 == 1 ){
             $(this).addClass("yellow");    
             $(this).addClass("ph"+n);    
