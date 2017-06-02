3874a9a8b78df09c83f447edb04a63c853b963e2
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-06-02 11:43:46 -0400

    St. Vital 1.0  /helpers.js

--- helpers.js
+++ helpers.js
@@ -122,7 +122,6 @@ $(window).load(function() {
         } 
     }
     
-    
     var today_hours = getTodaysHours();
     var hours = getPropertyHours();
     var feature_items = getFeatureList();
