43f7e74d7b91de03a63a40021478c6cf26bbf089
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-06-05 11:13:08 -0400

    St. Vital 1.0  /application.js

--- application.js
+++ application.js
@@ -358,7 +358,7 @@ function renderStoreDetails(container, template, collection, slug){
         
         //parse description_2
         var separate_des2 = val.description_2.match(/[^\r\n]+/g);
-        var design_desc = {};
+        var design_desc = [];
         var paragraph="";
         var title;
         $.each( separate_des2 , function( des2_key, des2_val ) {
