2fe093dc81c7faadca44cd246c60a736c43fd23f
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-06-05 10:37:38 -0400

    St. Vital 1.0  /application.js

--- application.js
+++ application.js
@@ -359,10 +359,10 @@ function renderStoreDetails(container, template, collection, slug){
         //parse description_2
         var separate_des2 = val.description_2.match(/[^\r\n]+/g);
         $.each( separate_des2 , function( des2_key, des2_val ) {
-            //if(des2_val != " ")
-           // {
+            if(des2_val != " ")
+            {
                 console.log(des2_key ,des2_val);
-          //  }
+            }
         });
         var rendered = Mustache.render(template_html,val);
         item_rendered.push(rendered);
