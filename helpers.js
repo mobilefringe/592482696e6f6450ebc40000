7ffd8372aca4aac07caa078eafbcb7893c241f18
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-07-10 12:42:34 -0400

    St. Vital 1.0  /helpers.js

--- helpers.js
+++ helpers.js
@@ -479,7 +479,7 @@ function getSVCSearchResults(search_string,max_results,trim_description_length){
                     };
     
     var genInfo =[];
-    var gen_info_list = []l
+    var gen_info_list = [];
     $.each( slug_list , function( key, each_list ) {
         $.each( each_list , function( key, slug ) {
             var pages_json = prefix+"/pages/" + slug + ".json"
