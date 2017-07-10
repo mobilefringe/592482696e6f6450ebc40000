b601241ca9e9038e3c24fdfd3a7a0d7900e7b0cb
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-07-10 13:17:33 -0400

    St. Vital 1.0  /helpers.js

--- helpers.js
+++ helpers.js
@@ -366,6 +366,18 @@ function getSVCSearchResults(search_string,max_results,trim_description_length){
                 return false;
             }
         }
+        if(store_ids.indexOf(val.id) == -1){
+            // console.log(search_string, val.name);
+            if(val.description_2.toLowerCase().indexOf(search_string.toLowerCase()) > -1){
+                val.description_trim = val.description_2.substring(0, trim_description_length) + "..";
+                stores.push(val);
+                store_ids.push(val.id);
+                count++;
+            }
+            if(count >= max_results){
+                return false;
+            }
+        }
     });
     search_results['stores'] = stores;
     if(stores.length === 0){
