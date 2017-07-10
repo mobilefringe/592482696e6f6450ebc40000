abd3170954974dccf1a9a32a10c291c6d079ae0d
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-07-10 12:27:34 -0400

    St. Vital 1.0  /helpers.js

--- helpers.js
+++ helpers.js
@@ -470,7 +470,7 @@ function getSVCSearchResults(search_string,max_results,trim_description_length){
     
     // St Vital Search through pages        
     var slug_list = { 
-                        'Service' : ['/pages/svc-customer-service','/pages/svc-leasing', :'/pages/svc-community-booth','/pages/svc-giftcard'],
+                        'Service' : ['/pages/svc-customer-service','/pages/svc-leasing', '/pages/svc-community-booth','/pages/svc-giftcard'],
                         'Submission Requirements' : ['/pages/svc-submission-requirements'],
                         'Base Building' : ['/pages/svc-base-building?building=1&menu=2','/pages/svc-base-building--2?building=1&menu=2','/pages/svc-base-building--3?building=1&menu=2','/pages/svc-base-building--4?building=1&menu=2','/pages/svc-base-building--5?building=1&menu=2','/pages/svc-base-building--6?building=1&menu=2'],
                         'Tenants Manual' : ['/pages/svc-tenants-manual'],
