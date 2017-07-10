e629102d9f056f35593d324c016ee4c72669cf86
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-07-10 12:36:49 -0400

    St. Vital 1.0  /helpers.js

--- helpers.js
+++ helpers.js
@@ -511,7 +511,7 @@ function getSVCSearchResults(search_string,max_results,trim_description_length){
     
     search_results['genInfo'] = jobs;
     if(genInfo.length === 0){
-        search_results['jobs_header_style'] = "display:none";
+        search_results['genInfo_header_style'] = "display:none";
     }
     search_results['summary'] = {"count":count};
     
