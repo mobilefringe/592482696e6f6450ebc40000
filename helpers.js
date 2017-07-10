1c7704441d3253d58563b70b84efcc981c1159ab
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-07-10 12:24:38 -0400

    St. Vital 1.0  /helpers.js

--- helpers.js
+++ helpers.js
@@ -499,7 +499,7 @@ function getSVCSearchResults(search_string,max_results,trim_description_length){
                         'Service' : ['/pages/svc-customer-service','/pages/svc-leasing', :'/pages/svc-community-booth','/pages/svc-giftcard'],
                         'Submission Requirements' : ['/pages/svc-submission-requirements'],
                         'Base Building' : ['/pages/svc-base-building?building=1&menu=2','/pages/svc-base-building--2?building=1&menu=2','/pages/svc-base-building--3?building=1&menu=2','/pages/svc-base-building--4?building=1&menu=2','/pages/svc-base-building--5?building=1&menu=2','/pages/svc-base-building--6?building=1&menu=2'],
-                        'Tenants Manual'
+                        'Tenants Manual' : ['/pages/svc-tenants-manual']
                         
                     };
     var all_genInfo = getJobsList();
