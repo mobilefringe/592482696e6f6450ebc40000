bde6b6a33c585b0661c202a86491cd1903ea965d
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-07-10 12:48:52 -0400

    St. Vital 1.0  /helpers.js

--- helpers.js
+++ helpers.js
@@ -496,7 +496,7 @@ function getSVCSearchResults(search_string,max_results,trim_description_length){
                     if((data.body.toLowerCase().indexOf(search_string.toLowerCase()) > -1) || (data.title.toLowerCase().indexOf(search_string.toLowerCase()) > -1)  ){
                         var val = {};
                         val.name = key;
-                        val.link = 
+                        val.link = slug;
                         val.description = data.body;
                         val.description_trim = val.description.substring(0, trim_description_length) + "..";
                         genInfo.push(val);
