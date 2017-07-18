aab90be7e8f2ee6cc02fd8eb0b323d3e74fbf6ac
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-07-18 11:08:44 -0400

    St. Vital 1.0  /helpers.js

--- helpers.js
+++ helpers.js
@@ -120,7 +120,7 @@ $(window).load(function() {
  
 function init(e){
     // $('<div class="modal-backdrop custom_backdrop"><img src="http://kodekloud.s3.amazonaws.com/sites/554a79236e6f64713f000000/69e8cd982124dc73de1f5a67a627ee75/loading.gif" class="" alt=""></div>').appendTo(document.body);
-    $('<div class="loader">Loading...</div>').appendTo(document.body);
+    $('<div class="modal-backdrop custom_backdrop"><div class="loader">Loading...</div>').appendTo(document.body);
     $(window).scroll(function(e){
 		if ($(this).scrollTop() > 100) {
 			$('.scrollToTop').fadeIn();
