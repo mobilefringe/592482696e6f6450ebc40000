d3eff7de06058564e12d802e68bac512d40b5bfc
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-07-18 10:40:09 -0400

    St. Vital 1.0  /application.js

--- application.js
+++ application.js
@@ -606,7 +606,7 @@ function renderJobDetails(container, template, collection){
         // var rendered = Mustache.render(template_html,val);
         // item_rendered.push(rendered);
     });
-    console.log(item_list);
+    //console.log(item_list);
     item_list.sort(function(a, b) {
         var textA = a.store_name.toUpperCase();
         var textB = b.store_name.toUpperCase();
