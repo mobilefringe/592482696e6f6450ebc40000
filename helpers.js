2c99f8909f11d923d042aa6824e5749f252c43c4
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-07-18 11:03:20 -0400

    St. Vital 1.0  /helpers.js

--- helpers.js
+++ helpers.js
@@ -191,7 +191,7 @@ function search_site(){
 
 function show_content(){
     $('.yield').fadeIn();
-    $(".modal-backdrop").remove();
+    // $(".modal-backdrop").remove();
     var windowWidth = $(window).width();
     if(windowWidth <= 1024) {
          $('.panel-collapse').removeClass('in')
